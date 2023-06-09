90d2c664d7d20e83d3169103a02dd99088216bb3
gobelynn <135753997+gobelynn@users.noreply.github.com>
2023-06-09 17:33:14 +0200

    Add files via upload

---
+++ js/phrases.js
@@ -0,0 +1,66 @@
+const RANDOM_QUOTE_API_URL = 'https://fraze.it/api/phrase/fr/a1632f3d-ac6d-4b68-a08a-5044400eca0c'
+const quoteDisplayElement = document.getElementById('quoteDisplay')
+const quoteInputElement = document.getElementById('quoteInput')
+const timerElement = document.getElementById('timer')
+let submit = document.querySelector('form button')
+
+quoteInputElement.addEventListener('input', () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+
+    let correct = true
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+    window.onkeyup = (key) => {
+        if (key.key == "Enter") {
+            if (correct === true) {
+                submit.click()
+            }
+        }}
+})
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+    .then(response => response.json())
+    .then(data => data.content)
+}
+
+async function renderNewQuote() {
+    const quote = await getRandomQuote()
+    quoteDisplayElement.innerHTML = ''
+    quote.split('').forEach(character => {
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
+    quoteInputElement.value = null
+    startTimer()
+}
+
+let startTime
+function startTimer() {
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) / 1000)
+}
+
+renderNewQuote()
